cf7c996fd6d6963575a788160343e55b2b3c4eb2
AndreySychev1917 <119424663+AndreySychev1917@users.noreply.github.com>
2022-12-06 11:57:10 +0400

    Add files via upload

---
+++ 15/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 15 про дни недели
+Console.Clear();
+Console.Write("ввведите день недели цифрой ");
+int d = int.Parse(Console.ReadLine());
+if ((d > 0) & (d <= 7 ))
+    if ((d > 0) & (d <6 ))
+    {
+    Console.WriteLine("Будний");
+    }
+    else
+    {Console.WriteLine("weeend");}
+else
+{
+  Console.WriteLine("Errrrrrrrooor");  
+}
